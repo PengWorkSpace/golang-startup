76ed5b2aca375ee3a54fb431d6c07d9602ae8114
liupeng <liupeng_internet@163.com>
2020-05-21 23:56:01 +0800

    变量定义、使用

---
+++ 变量定义、使用/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 定义全局变量
+var name = "akoncoder"
+var number = 100
+
+func main() {
+
+	innerStr := "inner akonc_coder"
+	innerNumber := 200
+
+	fmt.Println(name)
+	fmt.Println(number)
+
+	fmt.Println(innerStr)
+	fmt.Println(innerNumber)
+
+}
