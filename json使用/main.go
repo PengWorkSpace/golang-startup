4a31ce12b20413be8d72bb49bce9801520800e67
liupeng <liupeng_internet@163.com>
2020-05-24 00:36:30 +0800

    json序列化和反序列化使用

---
+++ json使用/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+
+	boy := person{
+		ID:      1,
+		Age:     28,
+		Name:    "akoncoder",
+		Address: "上海市",
+	}
+	jsonStr, err := json.Marshal(&boy)
+	if err != nil {
+		fmt.Println("json 序列化失败")
+	}
+
+	fmt.Printf("序列化后字符串=%v\n", string(jsonStr))
+
+	var boyStr person
+	err1 := json.Unmarshal([]byte(string(jsonStr)), &boyStr)
+	if err1 != nil {
+		fmt.Println("反序列化失败")
+	}
+
+	fmt.Println(boyStr)
+}
+
+type person struct {
+	ID      int
+	Age     int
+	Name    string
+	Address string
+}
