3ce29eb00f2b83b763e48d8b15a2a619504dbd9e
liupeng <liupeng_internet@163.com>
2020-07-07 16:18:11 +0800

    add channel

---
+++ channel/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//var ch = make(chan int, 2)
+	//ch <- 4
+	//num := <-ch
+	//fmt.Println(num)
+	//close(ch)
+	//fmt.Println("chan is closed")
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			ch1 <- i
+		}
+		close(ch1)
+	}()
+
+	go func() {
+		for {
+			i, ok := <-ch1
+			if !ok {
+				break
+			}
+			ch2 <- i * i
+		}
+		close(ch2)
+	}()
+
+	//通道关闭后会退出for range循环
+	for i := range ch2 {
+		fmt.Println(i)
+	}
+}
